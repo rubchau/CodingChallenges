5370f91e1de0de6926369a1a659541e30ed0280e
Ruby Chauhan <ruby.chau@berkeley.edu>
2020-08-08 14:32:16 -0700

    Organization.

--- src/HackerRank/HackerRank_Arrays.java
+++ src/HackerRank/HackerRank_Arrays.java
@@ -1,4 +1,4 @@
-/*
+package HackerRank;/*
  *
  * This class shows solutions to the HackerRank Arrays
  * Challenges from the "Interview Preparation Kit"
@@ -203,6 +203,34 @@ public class HackerRank_Arrays {
         }
 
         return swaps;
+    }
+
+    /**
+     * Starting with a 1-indexed array of zeros and a list of operations,
+     * for each operation add a value to each of the array element between
+     * two given indices, inclusive. Once all operations have been performed,
+     * return the maximum value in your array. For example, the length of
+     * your array of zeros is n = 10. Your list of queries is as follows:
+     *      a b k
+     *      1 5 3
+     *      4 8 7
+     *      6 9 1
+     * Add the values of k between indices a and b inclusive:
+     *          index -> 1  2  3  4  5  6  7  8  9  10
+     *       (original) [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+     *    (apply k = 3) [3, 3, 3, 3, 3, 0, 0, 0, 0, 0]
+     *   (apply k = 7) [3, 3, 3, 10, 10, 7, 7, 7, 0, 0]
+     *   (apply k = 1) [3, 3, 3, 10, 10, 8, 8, 8, 1, 0]
+     * After all operations are performed, the largest value
+     * in the array is 10.
+     * @param n the number of elements in the array
+     * @param queries a 2-dimensional array of queries where each queries[i]
+     *                contains three integers: a, b, k
+     * @return int maximum value in resulting array, after all operations applied
+     */
+    static long arrayManipulation(int n, int[][] queries) {
+
+        return 0;
     }
 
 
--- src/HackerRank/HackerRank_Warmup.java
+++ src/HackerRank/HackerRank_Warmup.java
@@ -1,3 +1,5 @@
+package HackerRank;
+
 import java.util.HashMap;
 import java.util.Map;
 
